e421efdae154d8bd3f7a810351ca841777a5dfe5
kafels <Marcelo.Effting@keyrus.com.br>
2021-06-06 19:54:50 -0300

    Improving install operation by checking if already exists some files or file content

--- install.sh
+++ install.sh
@@ -1,12 +1,29 @@
-echo "Storing your current configuration at '$HOME/.config/asound.state'"
-alsactl -f $HOME/.config/asound.state store
+ALSA_CONFIG_PATH=$HOME/.config/asound.state
+if [ ! -f "$ALSA_CONFIG_PATH" ]; then
+  echo "Storing your current configuration at '$ALSA_CONFIG_PATH'"
+  alsactl -f $ALSA_CONFIG_PATH store
+else
+  echo "File '$ALSA_CONFIG_PATH' already exists"
+fi
 
-echo "Adding run.sh permissions to run"
+echo "Adding run.sh file permissions to run"
 chmod +x run.sh
 
-echo "Creating a symlink at '$HOME/.local/bin/alsamixer-freeze'"
-ln -s "$(dirname "$(readlink -f "$0")")/run.sh" $HOME/.local/bin/alsamixer-freeze
+SYMLINK_PATH=$HOME/.local/bin/alsamixer-freeze
+if [ -f "$SYMLINK_PATH" ]; then
+  echo "Symlink '$SYMLINK_PATH' already exists. It's going to be recreate"
+  rm $SYMLINK_PATH
+fi
 
-echo "Enabling to automatically executes 'alsamixer-freeze' command after login"
-echo "# AlsaMixer Freeze" >> $HOME/.profile
-echo "alsamixer-freeze" >> $HOME/.profile
+echo "Creating a symlink at '$SYMLINK_PATH'"
+ln -s "$(dirname "$(readlink -f "$0")")/run.sh" $SYMLINK_PATH
+
+COMMAND_NAME="alsamixer-freeze"
+PROFILE_PATH=$HOME/.profile
+if ! grep -wq "$COMMAND_NAME" $HOME/.profile; then
+  echo "Enabling to automatically execute 'alsamixer-freeze' command after login"
+  echo "# AlsaMixer Freeze" >> $PROFILE_PATH
+  echo "alsamixer-freeze" >> $PROFILE_PATH
+else
+  echo "Command '$COMMAND_NAME' already exists in '$PROFILE_PATH'"
+fi
